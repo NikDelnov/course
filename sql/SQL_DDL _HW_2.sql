50a7069e68f1725edd4c253b685a9607668a8813
NikDelnov <Delen88@mail.ru>
2022-04-18 23:47:22 +0200

    edit hw_sql_2

--- sql/SQL_DDL _HW_2.sql
+++ sql/SQL_DDL _HW_2.sql
@@ -1,7 +1,13 @@
+--Создать таблицу employees
+--- id. serial,  primary key,
+--- employee_name. Varchar(50), not null
+
 create table employees(
 id serial primary key,
 employee_name varchar(50) not null
 )
+
+--Наполнить таблицу employee 70 строками.
 
 insert into employees(id, employee_name)
 values (default, 'Дмитрий Родин'),
@@ -75,10 +81,16 @@ values (default, 'Дмитрий Родин'),
 (default,'Варвара Маркина'),
 (default,'Ксения Бирюкова');
 
+--Создать таблицу salary
+--- id. Serial  primary key,
+--- monthly_salary. Int, not null
+
 create table salary(
 id serial PRIMARY KEY,
 monthly_salary int not null
 )
+
+--Наполнить таблицу salary 15 строками
 
 insert into salary (id, monthly_salary)
 values(default, 1000),
@@ -98,13 +110,19 @@ values(default, 1000),
 (default, 2400),
 (default, 2500);
 
+--Создать таблицу employee_salary
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique
+--- salary_id. Int, not null
+
 create table employee_salary(
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null
 )
 
-select * from employee_salary
+--Наполнить таблицу employee_salary 40 строками:
+--- в 10 строк из 40 вставить несуществующие employee_id
 
 insert into employee_salary(id, employee_id, salary_id)
 values(default, 300, 1),
@@ -148,13 +166,21 @@ values(default, 300, 1),
 (default, 42, 7),
 (default, 41, 8);
 	
+--Создать таблицу roles
+--- id. Serial  primary key,
+--- role_name. int, not null, unique
+
 create table roles(
 id serial primary key,
 role_name int not null unique
 )
 
+--Поменять тип столба role_name с int на varchar(30)
+
 alter table roles
 alter column role_name type varchar(30)
+
+--Наполнить таблицу roles 20 строками
 
 insert into roles(id, role_name)
 values(default, 'Junior Python developer'),
@@ -178,6 +204,11 @@ values(default, 'Junior Python developer'),
 (default, 'Senior Automation QA engineer'),
 (default, 'Senior Python developer');
 
+--Создать таблицу roles_employee
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+
 create table roles_employee(
 id serial primary key,
 employee_id int not null unique,
@@ -187,6 +218,8 @@ role_id int not null,
 foreign key (role_id)
 		references roles(id)
 )
+
+--Наполнить таблицу roles_employee 40 строками
 
 insert into roles_employee(id, employee_id, role_id)
 values (default, 40, 1),
@@ -229,4 +262,3 @@ values (default, 40, 1),
 		(default, 3, 19),
 		(default, 2, 20);
 	
-select * from roles_employee
